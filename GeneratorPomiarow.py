af95199caac291b84ff6b00285db96ede7939de6
mjan <@ja>
2021-03-16 14:00:45 +0100

    Praca zdalna

---
+++ GeneratorPomiarow.py
@@ -0,0 +1,8 @@
+import os
+import sys
+filepath = ".\Dane surowe"
+
+print(os.add_dll_directory(filepath))
+
+#with open('path/to/file.txt') as fp:
+    # do stuff with fp
---
+++ KonwerterPomiarow.py
@@ -0,0 +1,8 @@
+import os
+import sys
+filepath = sys.argv[0]
+
+print(filepath)
+
+#with open('path/to/file.txt') as fp:
+    # do stuff with fp
